ada1c31ede7a801601590d115b12cf8f1f9515a8
BORDEIANU Valentin <VBU4@dsone.3ds.com>
2020-02-05 17:29:54 +0100

    pretty

--- widgets/LiveTables/script/3dspace.js
+++ widgets/LiveTables/script/3dspace.js
@@ -1,5 +1,7 @@
-function _httpCallAuthenticated(url, options) {
-    require(["DS/WAFData/WAFData"], function(WAFData) {
+function _httpCallAuthenticated(url, options)
+{
+    require(["DS/WAFData/WAFData"], function(WAFData)
+    {
         WAFData.authenticatedRequest(url, options);
     });
 };
@@ -8,8 +10,8 @@ function _3dspace_get_csrf(host, docid, onDone = undefined, onError = undefined)
 {
     let url = host + `/resources/v1/modeler/documents/${docid}`;
 
-    _httpCallAuthenticated(url, 
-        {
+    _httpCallAuthenticated(url, {
+
             onComplete: function(response, headers, xhr) {
                 info = JSON.parse(response);
                 if (onDone) onDone(info.csrf.value);
@@ -18,6 +20,7 @@ function _3dspace_get_csrf(host, docid, onDone = undefined, onError = undefined)
             onFailure: function(response) {
                 if (onError) onError(response);
             }
+            
         }
     );
 }
@@ -25,10 +28,8 @@ function _3dspace_get_csrf(host, docid, onDone = undefined, onError = undefined)
 function _3dspace_file_url(host, docid, onDone = undefined, onError = undefined) 
 {
     let url = host + `/resources/v1/modeler/documents/${docid}/files/DownloadTicket`;
-    _3dspace_get_csrf(host, docid, function(csrf)
-    {
-        _httpCallAuthenticated(url, 
-            {
+    _3dspace_get_csrf(host, docid, function(csrf) {
+        _httpCallAuthenticated(url, {
                 method: "PUT",
                 headers: {ENO_CSRF_TOKEN: csrf}, 
     
