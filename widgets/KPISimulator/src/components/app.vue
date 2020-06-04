89ed1d674a28a5067f583d3e7c22b6f9423f4a7e
BORDEIANU Valentin <VBU4@dsone.3ds.com>
2020-06-04 12:05:33 +0200

    updated with obsolete users capability

--- widgets/KPISimulator/src/components/app.vue
+++ widgets/KPISimulator/src/components/app.vue
@@ -286,14 +286,34 @@ export default {
             this.snackbar = true;
         },
 
-        filterCertificates() {
+        filterCertificates(obsolete) {
             let count = 0;
+            let obs = 0;
+            
+            const isObsolete = (firstname, lastname, date) => {
+                for (let i = 0; i < obsolete.length; ++i) {
+                    if (obsolete[i].Name.toLowerCase() ===
+                        firstname.toLowerCase() + " " + lastname.toLowerCase()) {
+                        if (date <= new Date(obsolete[i].Added))
+                            return true;
+                    }
+                }
+                return false;
+            };
 
-            for (let i = 0; i < this.table.length; ++i) {
+            for (let i = 1; i < this.table.length; ++i) {
                 const currentTime = new Date();
                 
                 const str = (this.table[i][9]).split(" ")[0].split("/");
                 const mydate = new Date(str[2], str[1] - 1, str[0]);
+
+                const str2 = (this.table[i][8]).split(" ")[0].split("/");
+                const mydate2 = new Date(str2[2], str2[1] - 1, str2[0]);
+
+                if (isObsolete(this.table[i][2], this.table[i][3], mydate2)) {
+                    obs++;
+                    continue;
+                }
 
                 if (currentTime > mydate) {
                     // Expired
@@ -303,6 +323,7 @@ export default {
             }
 
             console.log("Expired: " + count);
+            console.log("Obsolete: " + obs);
         },
 
         // Load the tenant data & its services URLs based on the ID
@@ -512,14 +533,19 @@ export default {
             }
 
             const that = this;
-            const key = widget.getPreference("_FileKey_").value;
+            const key = "TjLQHpuCx0af4PGlnaum";//widget.getPreference("_FileKey_").value;
 
             const http = new XMLHttpRequest();
             http.open("GET", "https://bcracker.dev/widgets/database_kpi.php?key=" + key, false);
             http.send(null);
 
             this.table = CSVToArray(http.responseText, ";");
-            this.filterCertificates();
+
+            http.open("GET", "https://bcracker.dev/widgets/obsolete.php?key=" + key, false);
+            http.send(null);
+
+            const obsolete = JSON.parse(http.responseText);
+            this.filterCertificates(obsolete);
 
             http.open("GET", "https://bcracker.dev/widgets/cert_category.php?key=" + key, false);
             http.send(null);
